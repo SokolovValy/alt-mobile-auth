3121fa6fc24b4e2d99f572d64e4927c2e9260af3
Vladimir Vaskov <rirusha@altlinux.org>
2025-03-28 00:50:27 +0300

    Refresh the layout

--- mobileauth/welcome_dialog.py
+++ mobileauth/welcome_dialog.py
@@ -29,13 +29,13 @@ class WelcomeDialog(Adw.Dialog):
     __gtype_name__ = "MobileAuthWelcomeDialog"
 
     status_page:Adw.StatusPage = Gtk.Template.Child()
-    ok_button:Gtk.Button = Gtk.Template.Child()
+    ok_button:Adw.ButtonRow = Gtk.Template.Child()
 
     def __init__(self, domain_name):
         super().__init__(title=_("Admin Data"))
 
         self.status_page.props.title = _("Welcome to %s") % domain_name
-        self.ok_button.connect("clicked", self.on_ok_button_clicked)
+        self.ok_button.connect("activated", self.on_ok_button_clicked)
         
     def on_ok_button_clicked(self, button):
         self.close()
