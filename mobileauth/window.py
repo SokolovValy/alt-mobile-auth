6a490d8879f54b72c2ede5095c883548de821225
Vladimir Vaskov <rirusha@altlinux.org>
2025-03-28 00:26:33 +0300

    Fix entry row margin

--- mobileauth/window.py
+++ mobileauth/window.py
@@ -47,6 +47,8 @@ class Window(Adw.ApplicationWindow):
         self.host_name_entry.connect("notify::text", self._on_entry_changed)
         self.host_name_entry.props.text = GLib.get_host_name()
 
+        self.clear_host_name_button.bind_property("visible", self.clear_host_name_button.get_parent (), "visible", GObject.BindingFlags.SYNC_CREATE)
+
         self.submit_button.connect("clicked", self._on_submit)
 
         self.clear_host_name_button.connect("clicked", self._on_clear_host_name)
